7be2e363ee2ec56a199d82797d21d492cb4b6133
BoubouRSCL <abaut2001@gmail.com>
2024-02-19 14:41:51 +0100

    Third Commit

--- assets/pages/ProductsPage.jsx
+++ assets/pages/ProductsPage.jsx
@@ -1,10 +1,11 @@
 import axios from "axios";
 import React, {useState, useEffect} from "react";
-const ProductsPage = (props) => {
+import { Link } from "react-router-dom";
+const ProductsPage = () => {
     
-    const [products, setProducts] = useState()
+    const [products, setProducts] = useState([])
 
-    const getAllProducts = async (event) => {
+    const getAllProducts = async () => {
         try{
             const response = await axios.get("http://127.0.0.1:8000/api/products")
             setProducts(response.data['hydra:member'])
@@ -24,7 +25,16 @@ const ProductsPage = (props) => {
         <>
             <h1>Products</h1>
             <h3>Voir tous les produits</h3>
-            
+            {products.map((product) => (
+                <div className="card" style={{ width: "18rem" }} key={product.id}>
+                <img src={`/images/${product.image}`} alt={product.name} />
+                    <div className="card-body">
+                        <h5 className="card-title">{product.name}</h5>
+                        <p className="card-text">{product.description}</p>
+                        <Link to="/products" className="btn btn-primary">View Details</Link>
+                    </div>
+                </div>
+            ))}
         </>
      );
 }
